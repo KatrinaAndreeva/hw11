c68cbcbdab23ec70207806c484e8bc2b99ca8058
katrin <andreevak53@gmail.com>
2019-02-04 21:53:21 +0000

    saved

--- ajax.js
+++ ajax.js
@@ -1,20 +1,20 @@
 // Получить пользователей (users) от сервера
 //  https://jsonplaceholder.typicode.com используя класс
-//   созданный на занятии. Получив ответ от сервера вывести 
-//   имена пользователей на страницу. При клике на имя пользователя 
-//   в произвольном месте должна появиться подробная информация о нем. 
+//   созданный на занятии. Получив ответ от сервера вывести
+//   имена пользователей на страницу. При клике на имя пользователя
+//   в произвольном месте должна появиться подробная информация о нем.
 //   Для визуальной части можно использовать bootstrap или другие фреймворки.
 
 
-const url = 'https://jsonplaceholder.typicode.com';
+const url = 'https://jsonplaceholder.typicode.com/users';
 class CustomHttp {
     get(url, callback) {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.send();
+        xhr.open('GET', url, false);
         xhr.addEventListener('load', () => callback(xhr.responseText));
+        //xhr.addEventListener("load",uploadComplete,false);
+        xhr.send();
     }
-
     post(url, data, callback) {
         const xhr = new XMLHttpRequest();
         xhr.open('POST', url);
@@ -23,22 +23,40 @@ class CustomHttp {
         xhr.addEventListener('load', () => callback(xhr.responseText));
     }
 }
-
 const httpClient = new CustomHttp();
-
-// httpClient.get('https://jsonplaceholder.typicode.com/todos', (response) => {
-//     console.log(JSON.parse(response));
-// });
-
-// httpClient.get('https://jsonplaceholder.typicode.com/users', (response) => {
-//     console.log(JSON.parse(response));
-// });
-
-const data = {
-    body: 'Some body',
-    userId: 1,
-    title: 'Some title'
-};
-httpClient.post('https://jsonplaceholder.typicode.com/todos', JSON.stringify(data), (response) => {
-    console.log(response);
+httpClient.get('https://jsonplaceholder.typicode.com/users', (response) => {
+    let jsonRes = JSON.parse(response),
+        html = '';
+    //var data=jsonResponse.toSource();
+    html += '<div class="user_counts">' + jsonRes.length + '</div>';
+    for (i = 0; i < jsonRes.length; i++) {
+        html += '<div componentid="' + jsonRes[i].id + '" class="trow">';
+        html += '<span>' + jsonRes[i].name + '</span>';
+        html += '<div class="dropdown hide">';
+        html += '<div class="row">' + jsonRes[i].username + '</div>';
+        html += '<div class="row">' + jsonRes[i].email + '</div>';
+        html += '<div class="row">' + jsonRes[i].address.street + ' ' + jsonRes[i].address.suite + ' ' + jsonRes[i].address.city + ' ' + jsonRes[i].address.zipcode + '</div>';
+        html += '<div class="row">' + jsonRes[i].address.geo.lat + ' ' + jsonRes[i].address.geo.lng + '</div>';
+        html += '<div class="row">' + jsonRes[i].phone + '</div>';
+        html += '<div class="row">' + jsonRes[i].website + '</div>';
+        html += '<div class="row">' + jsonRes[i].company.name + ' ' + jsonRes[i].company.catchPhrase + ' ' + jsonRes[i].company.bs + '</div>';
+        html += '</div>';
+        html += '</div>';
+    }
+    document.getElementById("top_div_users").innerHTML = html;
 });
+document.addEventListener('click', function(e) {
+    if (e.target.tagName != 'SPAN') return;
+    // || e.target.nextElementSibling.tagName!='DIV'
+    let dropdown = e.target.nextElementSibling;
+    if (dropdown.classList.contains('hide')) {
+        let dropdownshow = document.querySelectorAll('.dropdown.show');
+        for (i = 0; i < dropdownshow.length; i++) {
+            dropdownshow[i].className = "dropdown hide";
+        }
+        dropdown.className = "dropdown show";
+    }
+    else {
+        dropdown.className = "dropdown hide";
+    }
+})
--- class.js
+++ class.js
@@ -4,11 +4,39 @@
 //     this.node = document.createElement(tagName);}
 // Пример вызова: const comp = new Component('span');
 
+class Component {
+    constructor(tagName = 'div') {
+        this.tagName = tagName;
+        this.node = document.createElement(tagName);
+    }
+}
+
+const comp = new Component('p');
+
+
+
 // 2.Реализовать конструктор и методы в ES6 синтаксисе:
 // function Component(tagName) {  this.tagName = tagName || 'div';
 // this.node = document.createElement(tagName);}
 // Component.prototype.setText = function (text) { 
 //  this.node.textContent = text;};
+
+class CreatingEl {
+    constructor(tagName = 'div') {
+        this.tagName = tagName;
+        this.node = document.createElement(tagName);
+    }
+    set addText(text) {
+        this.node.textContent = text;
+    }
+    get addText() {
+        return this.textContent;
+    }
+}
+
+const addTag = new CreatingEl('p');
+addTag.addText = 'some text';
+
 
 
 // 3.Создать класс калькулятора который будет принимать 
@@ -16,3 +44,66 @@
 // умножить , разделить. Также у него должны быть геттер и сеттер
 // для получения и установки текущего числа с которым производятся
 // вычисления.
+
+
+// class Calculate {
+//     constructor(value = 0) {
+//         this.number = value;
+//     }
+//     get number() {
+//         return this.number;
+//     }
+//     set number(value) {
+//         this.number = value;
+//     }
+
+// }
+
+class calkulator {
+    constructor(value = 0) {
+        this.number = value;
+    }
+    get_number() {
+        return this.number;
+    }
+    set_number(val) {
+        this.number = val;
+    }
+    fold(i, i2) {
+        if (!Number.isInteger(i)) return;
+        if (i2 == undefined || !Number.isInteger(i2)) {
+            i2 = i;
+            i = this.number;
+        }
+        return (i + i2);
+    }
+    minus(i, i2) {
+        if (!Number.isInteger(i)) return;
+        if (i2 == undefined || !Number.isInteger(i2)) {
+            i2 = i;
+            i = this.number;
+        }
+        return (i - i2);
+    }
+    divide(i, i2) {
+        if (!Number.isInteger(i)) return;
+        if (i2 == undefined || !Number.isInteger(i2)) {
+            i2 = i;
+            i = this.number;
+        }
+        return (i / i2);
+    }
+    multiply(i, i2) {
+        if (!Number.isInteger(i)) return;
+        if (i2 == undefined || !Number.isInteger(i2)) {
+            i2 = i;
+            i = this.number;
+        }
+        return (i * i2);
+    }
+}
+const calk = new calkulator(0);
+calk.set_number(10);
+console.log(calk.fold(5));
+console.log(calk.fold(5, 5));
+console.log(calk.multiply(5));
